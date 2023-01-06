6c61cb94d35622782a12e816704cb9232e4df215
Gökhan Yıldırım <82091624+thundergokhan@users.noreply.github.com>
2023-01-07 00:53:02 +0300

    Add files via upload

--- todolist/src/main/java/com/example/todoList/todolist/config/AppConfig.java
+++ todolist/src/main/java/com/example/todoList/todolist/config/AppConfig.java
@@ -5,15 +5,17 @@ import javax.persistence.EntityManagerFactory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.example.todoList.todolist.repository.JpaTodoRepository;
 import com.example.todoList.todolist.repository.TodoRepository;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.example.todoList.todolist.repository")
 public class AppConfig {
 
 	@Bean
 	public TodoRepository todoRepository() {
-		return (null);
+		return new JpaTodoRepository();
 	}
 }
